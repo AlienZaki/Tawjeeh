3f2e597c9bd003ac47192879bfc46c07d6b13891
AlienZaki <alien.zaki@yahoo.com>
2020-02-03 12:18:11 +0200

    database tables added

---
+++ database/migrations/2020_02_03_100034_create_clients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('cid');
+            $table->string('phone1');
+            $table->string('phone2')->default('')->nullable();
+            $table->string('barcode');
+            $table->boolean('s1')->default('0')->nullable();
+            $table->boolean('s2')->default('0')->nullable();
+            $table->boolean('s3')->default('0')->nullable();
+            $table->boolean('s4')->default('0')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
--- database/migrations/2020_02_03_100232_create_mobile_users_table.php
+++ database/migrations/2020_02_03_100232_create_mobile_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateMobileUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('mobile_users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('type')->default('user');
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('stage');
+            $table->string('photo');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('mobile_users');
     }
 }
